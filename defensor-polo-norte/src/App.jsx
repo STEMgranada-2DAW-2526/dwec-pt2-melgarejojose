bd1788c5d4698075681bcf8ee7dd799f937b44df
melgarejojose <melgarejoortegajosemiguel001.alu@stemgranada.com>
2025-11-25 11:24:27 +0100

    Sexto commit

--- defensor-polo-norte/src/App.jsx
+++ defensor-polo-norte/src/App.jsx
@@ -1,30 +1,13 @@
-const initialState = {
-  damageDealt: 0,
-  waveGoal: 0,
-  caramels: 20,
-  damagePerShot: 1,
-  autoShotsPerSecond: 1,
-  upgrades: [],
-};
+import './App.css'
 
-export default function App() {
-  function contadorReducer(state, action) {
-    if (action.type == 'CLICK_SHOOT'){
-
-    }else if (action.type == 'AUTO_SHOOT'){
-
-    }else if (action.type == 'BUY_MULTIPLIER'){
-      
-    }else if (action.type == 'BUY_DAMAGE_UPGRADE'){
-      
-    }else if (action.type == 'NEXT_WAVE'){
-      
-    }
-  }
+function App() {
 
   return (
     <>
-      
+      <GameProvider>
+        <Game/>
+        <UpgradesBoard/>
+      </GameProvider>
     </>
   )
 }
