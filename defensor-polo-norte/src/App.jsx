8a8a5c3627550c5285dcca56eed603f384c6c860
melgarejojose <melgarejoortegajosemiguel001.alu@stemgranada.com>
2025-11-25 10:52:37 +0100

    Segundo commit

--- defensor-polo-norte/src/App.jsx
+++ defensor-polo-norte/src/App.jsx
@@ -1,7 +1,16 @@
+const initialState = {
+  damageDealt: 0,
+  waveGoal: 0,
+  caramels: 20,
+  damagePerShot: 1,
+  autoShotsPerSecond: 1,
+  upgrades: [],
+};
+
 export default function App() {
   return (
     <>
-      
+
     </>
   )
 }
