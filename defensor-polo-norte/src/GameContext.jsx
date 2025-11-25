5111996711f4c09b1b572f7eebf43aed775383cd
melgarejojose <melgarejoortegajosemiguel001.alu@stemgranada.com>
2025-11-25 11:45:27 +0100

    Noveno commit

--- defensor-polo-norte/src/GameContext.jsx
+++ defensor-polo-norte/src/GameContext.jsx
@@ -1,9 +1,25 @@
 export const GameContext = createContext();
+const initialState = {
+    damageDealt: 0,
+    waveGoal: 100,
+    caramels: 20,
+    damagePerShot: 1,
+    autoshotPersecond: 1,
+    upgrades: [],
+};
 
-export default function GameProvider(){
+
+export default function GameProvider({ children }) {
+    function gameReducer(state, action) {
+
+        
+
+        return outputState;
+    }
+
     return (
-        <>
-
-        </>
+        <GameContext.Provider value={{ state, dispatch }}>
+            {children}
+        </GameContext.Provider>
     )
 }
